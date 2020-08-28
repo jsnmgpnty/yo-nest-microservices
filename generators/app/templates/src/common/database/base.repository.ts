11384b2a0fad323e6a8f49a0817f2524ffdde2b0
Jason Magpantay <jsnmgpnty@gmail.com>
2020-08-28 17:40:02 +0800

    cleaned up base repository

--- generators/app/templates/src/common/database/base.repository.ts
+++ generators/app/templates/src/common/database/base.repository.ts
@@ -12,40 +12,36 @@ export class BaseRepository<T extends BaseEntity> {
   public async create(item: T): Promise<T> {
     item.createdDate = moment.utc(new Date()).toDate();
     item.modifiedDate = moment.utc(new Date()).toDate();
-    return this.model.create(item) as Promise<T>;
+    return await this.model.create(item) as T;
   }
 
   public async getAll(): Promise<T[]> {
-    const result = this.model.find({});
-    return result.exec() as Promise<T[]>;
+    return await this.model.find({}) as T[];
   }
 
   public async update(id: string, item: T): Promise<T> {
     const parsedId = this.toObjectId(id);
     item.modifiedDate = new Date();
-    const result = this.model.update({ _id: parsedId }, item);
+    const result = await this.model.update({ _id: parsedId }, item);
     if (result.nModified !== 1) throw new Error(`Failed to update entity ${id}`);
     return this.findById(id);
   }
 
   public async delete(id: string): Promise<{ ok?: number; n?: number }> {
-    const result = this.model.remove({ _id: this.toObjectId(id) });
-    return result.exec();
+    return await this.model.remove({ _id: this.toObjectId(id) });
   }
 
   public async findById(id: string): Promise<T> {
-    const result = this.model.findById(id);
-    return result.exec() as Promise<T>;
+    return await this.model.findById(id) as T;
   }
 
   public async findOne(cond?: object): Promise<T> {
-    const result = this.model.findOne(cond);
-    return result.exec() as Promise<T>;
+    return await this.model.findOne(cond) as T;
   }
 
   public async find(cond?: object, options?: object, sort?: object, skip: number = 0, take: number = 10): Promise<T[]> {
-    const result = this.model.find(cond, options).skip(skip).limit(take);
-    return result.exec() as Promise<T[]>;
+    const result = await this.model.find(cond, options).skip(skip).limit(take).lean().exec();
+    return result as T[];
   }
 
   private toObjectId(id: string): mongoose.Types.ObjectId {
