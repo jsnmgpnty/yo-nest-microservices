ef1083e42e4f994997162342fe3fe2b84d434251
Jason Magpantay <jsnmgpnty@gmail.com>
2020-09-21 17:39:50 +0800

    added error handling on publish

--- generators/app/templates/src/messaging/publisher.service.ts
+++ generators/app/templates/src/messaging/publisher.service.ts
@@ -2,16 +2,30 @@ import { Injectable } from '@nestjs/common';
 import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
 import { isNil } from 'lodash';
 import { LoggerService } from '../logging';
-
 @Injectable()
 export class PublisherService {
-  constructor(private readonly amqpConnection: AmqpConnection, private logger: LoggerService) {}
+  constructor(private readonly amqpConnection: AmqpConnection, private logger: LoggerService) { }
 
   async publish<T>(exchange: string, routingKey: string, message: T) {
     return new Promise(resolve => {
-      this.logger.log(`AMQP published to ${exchange} - ${routingKey} | ${JSON.stringify(message)}`);
-      if (isNil(routingKey)) routingKey = '';
-      resolve(this.amqpConnection.publish(exchange, routingKey, message));
+      try {
+        this.logger.log(`AMQP published to ${exchange} - ${routingKey} | ${JSON.stringify(message)}`);
+        if (isNil(routingKey)) routingKey = '';
+        resolve(this.amqpConnection.publish(exchange, routingKey, message));
+      } catch (error) {
+        resolve(false);
+      }
+    });
+  }
+
+  async sendToQueue<T>(queue: string, message: T) {
+    return new Promise(resolve => {
+      try {
+        this.logger.log(`AMQP published to queue: ${queue} | ${JSON.stringify(message)}`);
+        resolve(this.amqpConnection.channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)), { persistent: true }));
+      } catch (error) {
+        resolve(false);
+      }
     });
   }
 }
